99b025bab1bb8273aa36f585b9ee2230f1ecf9dd
Kyle McLean <kyledmclean@gmail.com>
2020-05-23 02:25:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,46 @@
-require "pry"
-
 class Artist
+  attr_accessor :name, :songs
+  @@all = []  
 
-attr_accessor :name, :songs
-@@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def initialize(name)
-@name = name
-@songs = []
-@@all << self
-end
+  def add_song(song)
+    self.songs << song    
+                        
+  end
 
-def self.all
-  @@all
-end
+  def save
+    @@all << self     
+                  
+  end
 
-def add_song(song_name)
-self.songs << song_name
+  def self.all     
+    @@all
+  end
 
-end
+  def self.find_or_create_by_name(name)  
+                                        
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
 
-def self.find_or_create_by_name(name)
+  def self.find(name)    
+    self.all.detect { |artist| artist.name == name }
+  end
 
-self.all.select { |artist| artist.name == name }
+  def self.create(name)   
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
 
-
-end
-
-
+  def print_songs 
+    self.songs.each {|song| song.name}
+  end
 end
